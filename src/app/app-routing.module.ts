bf40cbf38c68bc2190721cd8405fc331152489c0
paula-narvaez <paulajavieranarvaez96@gmail.com>
2022-10-03 20:31:15 -0300

    commit2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'bienvenida',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'bienvenida',
+    loadChildren: () => import('./pages/bienvenida/bienvenida.module').then( m => m.BienvenidaPageModule)
+  },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  
   {
     path: 'inicio',
     loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
@@ -23,6 +36,8 @@ const routes: Routes = [
     path: 'otros',
     loadChildren: () => import('./pages/otros/otros.module').then( m => m.OtrosPageModule)
   },
+  
+  
 ];
 
 @NgModule({

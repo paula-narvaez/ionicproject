8723f620fcae921804458954f3c3b58743f96b29
paula-narvaez <paulajavieranarvaez96@gmail.com>
2022-10-03 22:54:12 -0300

    c4

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+
 
   constructor(
     public modalCtrl: ModalController,
@@ -20,6 +22,7 @@ export class LoginPage implements OnInit {
   async dismiss(){
     await this.modalCtrl.dismiss();
   }
+
 
 
 }
